ae2095611b3181afd3762e9f8271267f09146750
Javier C <jciocci@outlook.com>
2016-03-04 12:25:36 -0500

    update

--- resources/views/organization/edit.blade.php
+++ resources/views/organization/edit.blade.php
@@ -10,12 +10,21 @@
     <a class="nav-link active" href="#Details" data-toggle="tab">Details</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#Account" data-toggle="tab"> Users </a>
+    <a class="nav-link" href="#Account" data-toggle="tab"> Users  </a>
   </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#Waiver" data-toggle="tab"> Waiver  </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#Notifications" data-toggle="tab"> Notifications  </a>
+  </li>
+
 </ul>
 <div class="tab-content">
   <div class="tab-pane active" id="Details" style="padding-top: 10px" > @include('organization.form.edit') </div>
   <div class="tab-pane" id="Account"style="padding-top: 10px" > to be added </div>
+  <div class="tab-pane" id="Account"style="padding-top: 10px" > to be added </div>
+  <div class="tab-pane" id="Notifications"style="padding-top: 10px" > to be added </div>
 </div>
 
 @endsection
