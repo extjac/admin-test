8ef3ab2697c7da3c6b6ddcea9c2123fe991e69de
Javier C <jciocci@outlook.com>
2016-03-03 19:25:35 -0500

    updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,10 +6,12 @@
   <div class="page animsition vertical-align text-center">
     <div class="page-content vertical-align-middle">
       <div class="brand">
-        <h2 class="brand-text">admin</h2>
+        <h2 class="brand-text" align="center">
+            <i class="material-icons">person</i>
+        </h2>
       </div>
-      <p>Sign-in</p>
 
+      <p align="center">Sign-in</p>
 
         @if (count($errors) > 0)
             <div class="alert alert-danger">
@@ -25,27 +27,28 @@
         {!! csrf_field() !!}
 
 
-
             <div class="form-group">
-                    <input type="email" class="form-control " name="email" value="{{ old('email') }}" placeholder="Email" required="">
+                <input type="email" class="form-control " name="email" value="{{ old('email') }}" placeholder="Email" required>
             </div>
 
             <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Password" required>
+                <input type="password" class="form-control" name="password" placeholder="Password" required>
             </div>
 
             <div class="form-group">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember"> Remember Me
-                        </label>
-                    </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember"> Remember Me
+                    </label>
+                </div>
             </div>
 
-            <div class="form-group">
-                    <button type="submit" class="btn btn-primary">
-                        <i class="fa fa-btn fa-sign-in"></i> Login 
+            <div class="form-group ">
+                <div class="btn-group" >
+                    <button type="submit" class="btn btn-primary" align="center">
+                        <i class="material-icons">fingerprint</i> Login 
                     </button>
+                </div>
             </div>
         </form>
 
